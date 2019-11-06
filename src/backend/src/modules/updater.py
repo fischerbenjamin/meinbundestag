05f7fb27b2e9df5620e3b1ffe9a5f5bdd416d177
Benjamin Fischer <benjamin.f.fischer@fau.de>
2019-11-06 12:47:22 +0100

    updater module that updates speeches in the database

---
+++ src/backend/src/modules/updater.py
@@ -0,0 +1,35 @@
+"""
+@author: Benjamin Fischer
+"""
+
+
+# Local imports
+import src.modules.parsing as parsing
+import src.modules.processing as processing
+import src.modules.database as database
+import src.modules.config as config
+import src.modules.scraper as scraper
+
+
+# Global imports
+import wget
+import time
+
+
+def start(
+    is_test: bool = False, timeout: int = config.SCRAPER_DEFAULT_TIMEOUT,
+    interval: int = config.UPDATE_INTERVAL
+):
+    my_scraper = scraper.Scraper(timeout)
+    old_links, new_links = ([], [])
+    while True:
+        new_links = my_scraper.run()
+        work = [link for link in new_links if link not in old_links]
+        for link in work:
+            wget.download(link, config.TMP_FILE)
+            speeches = parsing.get_speeches(config.TMP_FILE)
+            for speech in speeches:
+                processing.analyze(speech)
+            database.insert_speeches(speeches, is_test)
+        old_links = new_links.copy()
+        time.sleep(interval)
