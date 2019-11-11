c9b8bddbf3d3fa4ebe996dd0ec0135bb65fa64b0
Benjamin Fischer <benjamin.f.fischer@fau.de>
2019-11-11 16:06:26 +0100

    Added test cases for config values.

--- src/backend/src/modules/config.py
+++ src/backend/src/modules/config.py
@@ -9,7 +9,7 @@ This module stores default variables for an easier use in other modules.
 import os
 
 
-DTD_FILE = os.path.abspath("../../data/dbtplenarprotokoll-data.dtd")
+DTD_FILE = os.path.abspath("data/dbtplenarprotokoll-data.dtd")
 SCRAPER_DEFAULT_TIMEOUT = 10
 TMP_FILE = "/tmp/protocol.xml"
 UPDATE_INTERVAL = 60*60*24
--- src/backend/test/test.py
+++ src/backend/test/test.py
@@ -1,17 +1,34 @@
-# tests/runner.py
+"""
+@author: Benjamin Fischer
+
+Testsuite for unit tests.
+"""
+
+
 import unittest
 
-# import your test modules
+
 import test.modules.parsing as parsing_test
+import test.modules.config as config_test
 
 
 def suite():
+    """
+    Create the testsuite.
+
+    Returns:
+        unittest.TestSuite: runnable testsuite containing all unit tests
+    """
     test_suite = unittest.TestSuite()
     test_suite.addTest(unittest.makeSuite(parsing_test.TestClass))
+    test_suite.addTest(unittest.makeSuite(config_test.TestClass))
     return test_suite
 
 
 def run():
+    """
+    Run the testsuite.
+    """
     all_suite = suite()
     runner = unittest.TextTestRunner(verbosity=3)
     runner.run(all_suite)
