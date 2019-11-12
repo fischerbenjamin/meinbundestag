cb138a8d369e893828807ad18212e7904c75c746
Benjamin Fischer <benjamin.f.fischer@fau.de>
2019-11-12 23:20:18 +0100

    use path relative to root directory

--- src/backend/src/modules/config.py
+++ src/backend/src/modules/config.py
@@ -10,6 +10,7 @@ import os
 
 
 DTD_FILE = os.path.abspath("data/dbtplenarprotokoll-data.dtd")
+SAMPLE_PROTOCOL = os.path.abspath("data/sample_protocol.xml")
 SCRAPER_DEFAULT_TIMEOUT = 10
 TMP_FILE = "/tmp/protocol.xml"
 UPDATE_INTERVAL = 60*60*24
