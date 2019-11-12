42244f72c6c1425b9527e57bcb532f9141e19494
Benjamin Fischer <benjamin.f.fischer@fau.de>
2019-11-12 23:25:03 +0100

    Removed test database and added unit test.

--- src/backend/src/modules/database.py
+++ src/backend/src/modules/database.py
@@ -3,7 +3,7 @@
 
 This module enables the communication with the mongo database that stores
 the speeches of the parliament. It provides an interface for
-requesting/inserting speeches. The suffix _t indicates test data.
+requesting/inserting speeches.
 """
 
 
@@ -17,8 +17,8 @@ import pymongo
 
 # Global variables
 client = None
+database = None
 speeches = None
-speeches_t = None
 
 
 def init(host: str, port: int) -> bool:
@@ -31,91 +31,79 @@ def init(host: str, port: int) -> bool:
     Returns:
         bool: True if initialization was successful, False otherwise
     """
-    global client, speeches, speeches_t
+    global client, database, speeches
     client = pymongo.MongoClient(host, port)
     try:
         client.admin.command('ismaster')
     except pymongo.errors.ConnectionFailure:
         return False
-    speeches = client.db.speeches
-    speeches_t = client.db.speeches_t
+    database = client["bundestag"]
+    speeches = database["speeches"]
 
 
-def get_speeches_for_user(name: str, db_test: bool = False) -> list:
+def get_speeches_for_user(name: str) -> list:
     """Return the speeches for a certain deputy.
 
     Args:
         name (str): deputy's name
-        db_test (bool, optional): Use test collection. Defaults to False.
 
     Returns:
         list: deputy's speeches (of type schema.Speech)
     """
-    my_speeches = __get_speeches_col(db_test)
+    global speeches
     my_query = {"name": name}
-    return list(my_speeches.find(my_query))
+    return list(speeches.find(my_query))
 
 
-def insert_speech(speech: schema.Speech, db_test: bool = False) -> bool:
+def insert_speech(speech: schema.Speech) -> bool:
     """Insert a single speech into the database.
 
     Args:
         speech (schema.Speech): speech object to insert
-        db_test (bool, optional): Use test collection. Defaults to False.
 
     Returns:
         bool: True if speech wasn't in database before, False otherwise
     """
-    if __speech_exists(speech, db_test):
+    global speeches
+    if __speech_exists(speech):
         return False
-    my_speeches = __get_speeches_col(db_test)
-    my_speeches.insert(repr(speech))
+    speeches.insert(speech.__dict__)
     return True
 
 
-def insert_speeches(speeches: list, db_test: bool = False) -> bool:
+def insert_speeches(speeches: list) -> bool:
     """Insert multiple speeches of type schema.Speech into the database.
 
     Args:
         speeches (list): list of speeches
-        db_test (bool, optional): Use test collection. Defaults to False.
 
     Returns:
-        bool: True if no speech was in database before, False otherwise
+        bool:   True if no given speech was in the database before,
+                False otherwise
     """
     result = []
     for speech in speeches:
-        result.append(insert_speech(speech, db_test))
+        result.append(insert_speech(speech))
     return all(result)
 
 
-def clear_speaches(db_test: bool = False) -> None:
-    """Clears the collection, e.g removes all speeches.
-
-    Args:
-        db_test (bool, optional): Use test collection. Defaults to False.
+def clear_speaches() -> None:
     """
-    my_speeches = __get_speeches_col(db_test)
-    my_speeches.drop()
+    Clears the collection, e.g removes all speeches.
+    """
+    global speeches
+    speeches.drop()
 
 
-def __speech_exists(speech: schema.Speech, db_test: bool = False) -> bool:
+def __speech_exists(speech: schema.Speech) -> bool:
     """Checks if the given speech is already in the database.
 
     Args:
         speech (schema.Speech): speech to check
-        db_test (bool, optional): Use test database. Defaults to False.
 
     Returns:
         bool: True if speech already exists, False otherwise
     """
-    my_speeches = __get_speeches_col(db_test)
+    global speeches
     my_query = {"speech_id": speech.speech_id}
-    return my_speeches.find(my_query).count() > 0
-
-
-def __get_speeches_col(db_test: bool) -> pymongo.collection.Collection:
-    global speeches, speeches_t
-    if speeches is None or speeches_t is None:
-        raise Exception("Must call init() before using this module.")
-    return speeches_t if db_test else speeches
+    return speeches.find(my_query).count() > 0
--- src/backend/test/modules/database.py
+++ src/backend/test/modules/database.py
@@ -1,3 +1,32 @@
 """
 @author: Benjamin Fischer
 """
+
+
+import mockupdb
+import unittest
+
+
+import src.modules.database as database_src
+
+
+class TestClass(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.server = mockupdb.MockupDB(auto_ismaster={"maxWireVersion": 3})
+        cls.server.run()
+        database_src.init(cls.server.uri, cls.server.port)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.server.stop()
+
+    def test_client_not_none(self):
+        self.assertIsNotNone(database_src.client)
+
+    def test_database_not_none(self):
+        self.assertIsNotNone(database_src.database)
+
+    def test_collection_not_none(self):
+        self.assertIsNotNone(database_src.speeches)
