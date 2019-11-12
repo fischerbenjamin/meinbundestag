74231159fc5651a0a4c74bad3992bf3eb2a50eeb
Benjamin Fischer <benjamin.f.fischer@fau.de>
2019-11-12 23:24:27 +0100

    Added unit test for processing module

--- src/backend/test/modules/processing.py
+++ src/backend/test/modules/processing.py
@@ -1,3 +1,31 @@
 """
 @author: Benjamin Fischer
 """
+
+
+import unittest
+
+
+import src.modules.processing as processing_src
+import src.modules.schema as schema_src
+
+
+class TestClass(unittest.TestCase):
+
+    def setUp(self):
+        self.dummy_speech = schema_src.Speech.dummy_speech()
+        self.random_speech = schema_src.Speech.random_speech()
+
+    def test_analyze_dummy_speech(self):
+        processing_src.analyze(self.dummy_speech)
+        self.assertIsNotNone(self.dummy_speech.analysis)
+        self.assertListEqual(self.dummy_speech.comments, [])
+        self.assertEqual(self.dummy_speech.analysis["no_comments"], 0)
+        self.assertEqual(self.dummy_speech.analysis["polarity"], 0.0)
+        self.assertEqual(self.dummy_speech.analysis["subjectivity"], 0.0)
+
+    def test_analyze_random_speech(self):
+        processing_src.analyze(self.random_speech)
+        self.assertIsNotNone(self.random_speech.analysis)
+        self.assertNotEqual(self.random_speech.comments, [])
+        self.assertNotEqual(self.random_speech.analysis["no_comments"], 0)
