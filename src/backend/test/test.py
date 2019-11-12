50b25ee5bfe19c34b32ed37c8f3de2f5370f60b1
Benjamin Fischer <benjamin.f.fischer@fau.de>
2019-11-12 23:25:22 +0100

    Added new unit tests to test suite.

--- src/backend/test/test.py
+++ src/backend/test/test.py
@@ -10,6 +10,9 @@ import unittest
 
 import test.modules.parsing as parsing_test
 import test.modules.config as config_test
+import test.modules.scraper as scraper_test
+import test.modules.database as database_test
+import test.modules.processing as processing_test
 
 
 def suite():
@@ -22,6 +25,9 @@ def suite():
     test_suite = unittest.TestSuite()
     test_suite.addTest(unittest.makeSuite(parsing_test.TestClass))
     test_suite.addTest(unittest.makeSuite(config_test.TestClass))
+    #test_suite.addTest(unittest.makeSuite(scraper_test.TestClass))
+    test_suite.addTest(unittest.makeSuite(database_test.TestClass))
+    test_suite.addTest(unittest.makeSuite(processing_test.TestClass))
     return test_suite
 
 
