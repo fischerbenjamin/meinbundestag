f1e6856e62ac378369583863af6011309aa21f74
Benjamin Fischer <benjamin.f.fischer@fau.de>
2019-11-15 20:49:31 +0100

    Fixed failing test case.

--- src/backend/test/modules/parsing.py
+++ src/backend/test/modules/parsing.py
@@ -5,7 +5,6 @@ import src.modules.parsing as parsing
 import src.modules.config as config
 
 
-
 class TestClass(unittest.TestCase):
 
     def test_get_speeches_filepath_none(self):
@@ -29,5 +28,5 @@ class TestClass(unittest.TestCase):
             list(parsing.get_speeches("./parsing.py"))
 
     def test_get_speeches_filepath_valid(self):
-        speeches = list(parsing.get_speeches(SAMPLE_PROTOCOL))
+        speeches = list(parsing.get_speeches(config.SAMPLE_PROTOCOL))
         self.assertGreater(len(speeches), 0)
