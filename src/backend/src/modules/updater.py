8a4ad767f251a5400723ba8851c0e54d13706bce
Benjamin Fischer <benjamin.f.fischer@fau.de>
2019-11-18 17:04:13 +0100

    Added logging and exception handling.

--- src/backend/src/modules/updater.py
+++ src/backend/src/modules/updater.py
@@ -7,27 +7,37 @@
 import src.modules.parsing as parsing
 import src.modules.database as database
 import src.modules.processing as processing
+import src.modules.myexceptions as myexceptions
 
 
 # Global imports
 import os
 import wget
+import logging
 import threading
 
 
 class Updater(threading.Thread):
 
-    def __init__(self, semaphore: threading.Semaphore):
-        self.semaphore = semaphore
+    def __init__(self, sem: threading.Semaphore):
+        self.sem = sem
 
     def run(self):
         while True:
-            self.semaphore.acquire()
+            logging.info("Updater requests semaphore.")
+            self.sem.acquire()
+            logging.info("Updater obtained semaphore.")
             protocol = database.get_protocol_to_process()
             fpath = os.path.join("/protocols", protocol.fname)
             wget.download(protocol.url, fpath)
             speeches = parsing.get_speeches(fpath)
-            for speech in speeches:
-                processing.analyze(speech)
+            try:
+                processing.analyze_speeches(speeches)
+            except myexceptions.SpeechAnalysisException:
+                logging.error(
+                    "Failed analyzing speech in protocol '{}'".format(
+                        protocol.url
+                    )
+                )
             database.protocol_is_done(protocol)
             database.insert_speeches(speeches)
