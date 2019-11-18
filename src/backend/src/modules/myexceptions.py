54c2f2d00786eebb281c6b0e7bb40d0a012f07f1
Benjamin Fischer <benjamin.f.fischer@fau.de>
2019-11-18 17:02:21 +0100

    Added custom exception for failing during speech analysis and json serialization.

--- src/backend/src/modules/myexceptions.py
+++ src/backend/src/modules/myexceptions.py
@@ -7,3 +7,11 @@ This module contains all user defined exceptions.
 
 class SpeechParsingException(Exception):
     pass
+
+
+class JSONInitializationException(Exception):
+    pass
+
+
+class SpeechAnalysisException(Exception):
+    pass
