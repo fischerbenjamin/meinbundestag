eaa16bf8b1116b167a6ae5c1a4750db4d5307d5c
Benjamin Fischer <benjamin.f.fischer@fau.de>
2019-11-28 09:03:51 +0100

    Applied linting. Changed structure of Speech and EnvVars.

--- src/backend/src/modules/schema.py
+++ src/backend/src/modules/schema.py
@@ -1,119 +1,169 @@
-"""
-@author: Benjamin Fischer
+"""Typedef of custom objects.
 
 This module defines the structure of all objects used in the backend that are
 no python built-in types.
 """
 
 
+# Global imports
+from typing import Dict, Any, Union, List, Tuple
+
+
+# Local imports
 import src.modules.myexceptions as myexceptions
 
 
 class JSONSerializable:
+    """Base class for all classes which are stored in the database."""
 
     def to_json(self) -> dict:
-        """Child classes must provide an implementation of this method in order
+        """Convert to json data.
+
+        Child classes must provide an implementation of this method in order
         to be stored in the database.
 
         Raises:
             NotImplementedError: override by child classes
+
         """
         raise NotImplementedError
 
-    def from_json(self) -> "JSONSerializable":
-        """Child classes must provide an implementation of this method in order
+    def from_json(self, obj: dict) -> "JSONSerializable":
+        """Initialize from json data.
+
+        Child classes must provide an implementation of this method in order
         to be initialized from the database.
+
+        Args:
+            obj (dict): json data to load from
 
         Raises:
             NotImplementedError: override by child classes
+
         """
         raise NotImplementedError
 
     @staticmethod
     def assert_keys(keys: list, obj: dict):
-        if not all([True if k in obj.keys() else False for k in keys]):
-            raise myexceptions.JSONInitializationException
+        """Assert that the object has all required keys.
+
+        Args:
+            keys (list): required keys
+            obj (dict): object to convert
+
+        Raises:
+            myexceptions.JSONInitializationException: if key is not in object
+
+        """
+        for k in keys:
+            if k not in obj.keys():
+                raise myexceptions.JSONInitializationException
 
 
 class SpeechParagraph(JSONSerializable):
-
-    """
-    This class defines the structure of a single paragraph of a speech.
-    """
+    """Structure of a single paragraph of a speech."""
 
     TYPE_SPEECH = "speech"
     TYPE_COMMENT = "comment"
 
-    def __init__(self, type: str, text: str):
-        self.type = type
+    def __init__(self, type_of_paragraph: str, text: str):
+        """Initialize object.
+
+        Args:
+            type (str): type of the paragraph
+            text (str): text of the paragraph
+
+        """
+        self.type_of_paragraph = type_of_paragraph
         self.text = text
 
     def get_text(self) -> str:
-        """Return the text of the paragraph. Replacing None values with empty
-        string.
+        """Return the text of the paragraph.
+
+        None values are replaced with the empty string.
 
         Returns:
             str: text of the paragraph
+
         """
         return "" if self.text is None else self.text
 
     def is_speech(self) -> bool:
-        """Returns True if the paragraph is part of the speech or a question,
-        False otherwise
+        """Check if the paragraph is actual speech text.
 
         Returns:
-            bool: paragraph is no comment
+            bool: True if the paragraph is part of the speech, False otherwise
+
         """
-        return self.type == SpeechParagraph.TYPE_SPEECH
+        return self.type_of_paragraph == SpeechParagraph.TYPE_SPEECH
 
     def is_comment(self) -> bool:
-        """Returns True if the paragrapg is a comment, False otherwise.
+        """Check if the paragraph is a comment.
 
         Returns:
-            bool: paragraph is comment
-        """
-        return self.type == SpeechParagraph.TYPE_COMMENT
+            bool: True if the paragraph is a comment, False otherwise
 
-    def to_json(self) -> dict:
+        """
+        return self.type_of_paragraph == SpeechParagraph.TYPE_COMMENT
+
+    def to_json(self) -> Dict[str, str]:
+        """Convert object to json data.
+
+        Returns:
+            Dict[str, str]: mapping of attributes
+
+        """
         return dict(
-            type=self.type,
+            type=self.type_of_paragraph,
             text=self.text
         )
 
     @classmethod
-    def from_json(cls, obj: dict) -> "SpeechParagraph":
-        SpeechParagraph.assert_keys(["type", "text"], obj)
+    def from_json(cls, obj: Dict[str, Any]) -> "SpeechParagraph":
+        """Initialize an object from json data.
+
+        Returns:
+            SpeechParagrah: object
+
+        """
+        SpeechParagraph.assert_keys(["type_of_paragraph", "text"], obj)
         return cls(
-            type=obj["type"],
+            type_of_paragraph=obj["type_of_paragraph"],
             text=obj["text"]
         )
 
 
 class Speech(JSONSerializable):
-
-    """
-    This class defines the structure of a 'Speech' object.
-    """
+    """Structure of a single speech."""
 
     def __init__(
-        self, name: str, party: str, topic: str, date: str,
-        content: "SpeechContent", speaker_id: str, speech_id: str
+            self, meta: Dict[str, str],
+            content: "SpeechContent", speaker_id: str, speech_id: str
     ):
-        self.name = name
-        self.party = party
-        self.topic = topic
-        self.date = date
+        """Init object.
+
+        Args:
+            meta (Dict[str, str]): (name, party, topic, date)
+            content (SpeechContent): content of the speech
+            speaker_id (str): speaker's ID
+            speech_id (str): ID of the speech
+
+        """
+        self.meta = meta
         self.content = content
         self.speaker_id = speaker_id
         self.speech_id = speech_id
         self.analysis = SpeechAnalysis()
 
-    def to_json(self):
+    def to_json(self) -> Dict[str, Any]:
+        """Convert object to json data.
+
+        Returns:
+            Dict[str, Any]: mapping of attributes
+
+        """
         return dict(
-            name=self.name,
-            party=self.party,
-            topic=self.topic,
-            date=self.date,
+            meta=self.meta,
             content=self.content.to_json(),
             speaker_id=self.speaker_id,
             speech_id=self.speech_id,
@@ -122,52 +172,64 @@ class Speech(JSONSerializable):
 
     @classmethod
     def from_json(cls, obj: dict) -> "Speech":
+        """Initialize an object from json data.
+
+        Returns:
+            Speech: object
+
+        """
         Speech.assert_keys([
-            "name", "party", "topic", "date", "content", "speaker_id",
-            "speech_id", "analysis"
+            "meta", "content", "speaker_id", "speech_id"
         ], obj)
         return cls(
-            name=obj["name"],
-            party=obj["party"],
-            topic=obj["topic"],
-            date=obj["date"],
+            meta=obj["meta"],
             content=SpeechContent.from_json(obj["content"]),
             speaker_id=obj["speaker_id"],
-            speech_id=obj["speech_id"],
-            analysis=SpeechAnalysis.from_json(obj["analysis"])
+            speech_id=obj["speech_id"]
         )
 
 
 class SpeechAnalysis(JSONSerializable):
-
-    """
-    This class defines the structure of the analysis of a speech.
-    Analysis is done by the 'processing' module.
-    """
+    """Structure of the analysis of a speech."""
 
     def __init__(
-        self, polarity: float = 0.0, subjectivity: float = 0.0,
-        number_of_comments: int = 0
+            self, polarity: float = 0.0, subjectivity: float = 0.0,
+            number_of_comments: int = 0
     ):
-        self.polarity = 0.0
-        self.subjectivity = 0.0
-        self.number_of_comments = 0
-
-    def update(
-        self, polarity: float, subjectivity: float, number_of_comments: int
-    ) -> None:
-        """Updates the values of the SpeechAnalysis object.
+        """Init object.
 
         Args:
-            polarity (float): polarity of the speech
-            subjectivity (float): subjectivity of the speech
-            number_of_comments (int): number of comments during the speech
+            polarity (float, optional): polarity. Defaults to 0.0.
+            subjectivity (float, optional): subjectivity. Defaults to 0.0.
+            number_of_comments (int, optional): #comments. Defaults to 0.
+
         """
         self.polarity = polarity
         self.subjectivity = subjectivity
         self.number_of_comments = number_of_comments
 
-    def to_json(self) -> dict:
+    def update(
+            self, polarity: float, subjectivity: float, number_of_comments: int
+    ) -> None:
+        """Update the values of the current object.
+
+        Args:
+            polarity (float): polarity of the speech
+            subjectivity (float): subjectivity of the speech
+            number_of_comments (int): number of comments during the speech
+
+        """
+        self.polarity = polarity
+        self.subjectivity = subjectivity
+        self.number_of_comments = number_of_comments
+
+    def to_json(self) -> Dict[str, float]:
+        """Convert object to json data.
+
+        Returns:
+            Dict[str, float]: mapping of attributes
+
+        """
         return dict(
             polarity=self.polarity,
             subjectivity=self.subjectivity,
@@ -176,6 +238,12 @@ class SpeechAnalysis(JSONSerializable):
 
     @classmethod
     def from_json(cls, obj: dict) -> "SpeechAnalysis":
+        """Initialize object from json data.
+
+        Returns:
+            SpeechAnalysis: object
+
+        """
         SpeechAnalysis.assert_keys([
             "polarity", "subjectivity", "number_of_comments"
         ], obj)
@@ -187,18 +255,28 @@ class SpeechAnalysis(JSONSerializable):
 
 
 class Protocol(JSONSerializable):
-
-    """
-    This class defines the structure of a protocol. It stores the url and
-    filename and a flag whether the protocol has already been processed or not.
-    """
+    """Structure of a protocol."""
 
     def __init__(self, url: str, fname: str, done: bool = False):
+        """Init object.
+
+        Args:
+            url (str): url of the protocol file
+            fname (str): filename of the protocol
+            done (bool, optional): processed or not. Defaults to False.
+
+        """
         self.url = url
         self.fname = fname
         self.done = done
 
-    def to_json(self) -> dict:
+    def to_json(self) -> Dict[str, Union[str, bool]]:
+        """Convert object to json data.
+
+        Returns:
+            Dict[str, Union[str, bool]]: mapping of attributes
+
+        """
         return dict(
             url=self.url,
             fname=self.fname,
@@ -207,6 +285,12 @@ class Protocol(JSONSerializable):
 
     @classmethod
     def from_json(cls, obj: dict) -> "Protocol":
+        """Initialize object from json data.
+
+        Returns:
+            Protocol: object
+
+        """
         Protocol.assert_keys(["url", "fname", "done"], obj)
         return cls(
             url=obj["url"],
@@ -216,37 +300,61 @@ class Protocol(JSONSerializable):
 
 
 class SpeechEntry(JSONSerializable):
+    """Structure of a speech entry.
 
-    """
-    This class defines the structure of a speech entry. An entry is a set of
-    subsequent paragraphs of one deputy. The deputy can be either be the
-    speaker himself, another deputy or the president of the parliament.
+    An entry is a set of subsequent paragraphs of one deputy.
+    The deputy can be either be the speaker himself, another deputy or the
+    president of the parliament.
     """
 
-    def __init__(self, speaker: str, is_speaker: bool, paragraphs: list):
+    def __init__(
+            self, speaker: str, is_speaker: bool,
+            paragraphs: List["SpeechParagraph"]
+    ):
+        """Init object.
+
+        Args:
+            speaker (str): deputy's name
+            is_speaker (bool): deputy is speaker or not
+            paragraphs (List[SpeechParagraph]): list of subsequent paragraphs
+
+        """
         self.speaker = speaker
         self.is_speaker = is_speaker
         self.paragraphs = paragraphs
 
     def add_paragraph(self, paragraph: SpeechParagraph) -> None:
-        """Adds an paragraph to the speech entry.
+        """Add an paragraph to the speech entry.
+
+        Append the paragraph to the list of paragraphs.
 
         Args:
             paragraph (SpeechParagraph): paragraph to add
+
         """
         self.paragraphs.append(paragraph)
 
-    def to_json(self) -> dict:
+    def to_json(self) -> Dict[str, Union[str, bool, List[SpeechParagraph]]]:
+        """Convert object to json data.
+
+        Returns:
+            Dict[str, Union[str, bool, List[SpeechParagraph]]]: attributes
+
+        """
         return dict(
             speaker=self.speaker,
             is_speaker=self.is_speaker,
-            paragraphs=list(
-                paragraph.to_json() for paragraph in self.paragraphs
-            )
+            paragraphs=list(p.to_json() for p in self.paragraphs)
         )
 
     @classmethod
     def from_json(cls, obj) -> "SpeechEntry":
+        """Initialize object from json data.
+
+        Returns:
+            SpeechEntry: object
+
+        """
         SpeechEntry.assert_keys(["speaker", "is_speaker", "paragraphs"], obj)
         return cls(
             speaker=obj["speaker"],
@@ -259,29 +367,39 @@ class SpeechEntry(JSONSerializable):
 
 
 class SpeechContent:
+    """Structure of the content of a single speech.
 
-    """
-    This class defines the structure of the contents of a single speech.
     The content is divided into objects of type SpeechEntry that contain
     subsequent paragraphs of a deputy.
     """
 
-    def __init__(self, entries: list):
+    def __init__(self, entries: List[SpeechEntry]):
+        """Init object.
+
+        Args:
+            entries (List[SpeechEntry]): entries of the speech (so far)
+
+        """
         self.entries = entries
 
     def add_speech_entry(self, speech_entry: SpeechEntry) -> None:
-        """Adds a single speech entry to the content of the speech.
+        """Add a single speech entry to the content of the speech.
 
         Args:
             speech_entry (SpeechEntry): speech entry to add
+
         """
         self.entries.append(speech_entry)
 
     def get_speakers_text(self) -> str:
-        """Returns the text of the speaker.
+        """Return the text of the speaker.
+
+        Iterate over all paragraphs and check if the text of the paragraph
+        was spoken by the speaker.
 
         Returns:
             str: text of the speech (without questions and comments)
+
         """
         return " ".join([
             speech_paragraph.get_text()
@@ -290,11 +408,12 @@ class SpeechContent:
             if speech_paragraph.is_speech() and speech_entry.is_speaker
         ])
 
-    def get_comments(self) -> list:
-        """Returns all comments of the speech
+    def get_comments(self) -> List[str]:
+        """Return all comments during the speech.
 
         Returns:
-            list: comments
+            List[str]: the text of all comments during the speech
+
         """
         return [
             speech_paragraph.get_text()
@@ -303,13 +422,23 @@ class SpeechContent:
             if speech_paragraph.is_comment()
         ]
 
-    def to_json(self) -> dict:
-        return list(
-            speech_entry.to_json() for speech_entry in self.entries
-        )
+    def to_json(self) -> List[Any]:
+        """Convert object to json data.
+
+        Returns:
+            List[Any]: list containing json representation of speech entries
+
+        """
+        return list(speech_entry.to_json() for speech_entry in self.entries)
 
     @classmethod
     def from_json(cls, obj) -> "SpeechContent":
+        """Initialize object from json data.
+
+        Returns:
+            SpeechContent: object
+
+        """
         SpeechEntry.assert_keys(["entries"], obj)
         return cls(
             entries=list(
@@ -319,14 +448,105 @@ class SpeechContent:
         )
 
 
-class SpeechIndex:
+class SpeechIndex(JSONSerializable):
+    """Structure of an speech index object.
 
-    """
-    This class defines the structure of an index object that is used by the
-    module @parsing. It helps dividing the speech into a list of subsequent
-    paragraphs of a single deputy.
+    Used for parsing a speech. It helps dividing the speech into a list of
+    subsequent paragraphs of a single deputy.
     """
 
     def __init__(self, index: int, name: str):
+        """Init object.
+
+        Args:
+            index (int): index of element
+            name (str): deput's name
+
+        """
         self.index = index
         self.name = name
+
+    def to_json(self) -> Dict[str, Union[int, str]]:
+        """Convert object to json data.
+
+        Returns:
+            Dict[str, Union[int, str]]: mapping of attributes
+
+        """
+        return dict(index=self.index, name=self.name)
+
+    @classmethod
+    def from_json(cls, obj: dict) -> "SpeechIndex":
+        """Initialize object from json data.
+
+        Returns:
+            SpeechIndex: object
+
+        """
+        SpeechEntry.assert_keys(["index", "name"], obj)
+        return cls(index=int(obj["index"]), name=obj["name"])
+
+
+class EnvVars(JSONSerializable):
+    """Structure of environment variables used by the backend."""
+
+    # pylint: disable=too-many-arguments
+    def __init__(
+            self,
+            database_config: Tuple[str, int, bool],
+            api_config: Tuple[str, int],
+            scraper_config: Tuple[int, int],
+            protocol_config: Tuple[str, str],
+            other_config: Tuple[Any, ...]
+    ):
+        """Init object.
+
+        Args:
+            database_config (Tuple[str, int, bool]): (host, port, clear)
+            api_config (Tuple[str, int]): (host, port)
+            scraper_config (Tuple[int, int]): (timeout, interval)
+            protocol_config (Tuple[str, str]): (dtd file, destination)
+            other_config (Tuple[Any, ...]): (logging level)
+
+        """
+        self.database_config = database_config
+        self.api_config = api_config
+        self.scraper_config = scraper_config
+        self.protocol_config = protocol_config
+        self.other_config = other_config
+    # pylint: enable=too-many-arguments
+
+    def to_json(self) -> Dict[str, Any]:
+        """Convert object to json data.
+
+        Returns:
+            Dict[str, Any]: mapping of attributes
+
+        """
+        return dict(
+            database_config=self.database_config,
+            api_config=self.api_config,
+            scraper_config=self.scraper_config,
+            protocol_config=self.protocol_config,
+            other_config=self.other_config
+        )
+
+    @classmethod
+    def from_json(cls, obj: Dict[str, Any]) -> "EnvVars":
+        """Initialize object from json data.
+
+        Returns:
+            EnvVars: object
+
+        """
+        EnvVars.assert_keys([
+            "database_config", "api_config", "scraper_config",
+            "protocol_config", "other_config"
+        ], obj)
+        return cls(
+            database_config=obj["database_config"],
+            api_config=obj["api_config"],
+            scraper_config=obj["scraper_config"],
+            protocol_config=obj["protocol_config"],
+            other_config=obj["other_config"]
+        )
