4140a6243d8819ebd15d1ed9877524f29b963cc6
Benjamin Fischer <benjamin.f.fischer@fau.de>
2019-11-29 13:02:24 +0100

    Updated test suite.

--- src/backend/test/test.py
+++ src/backend/test/test.py
@@ -1,40 +1,36 @@
-"""
-@author: Benjamin Fischer
+"""Testsuite for unittests.
 
-Testsuite for unit tests.
+This module is used as a test suite and runs all unit tests.
 """
 
+
+# Python imports
 import sys
 import unittest
 
 
+# Local imports
 import test.modules.parsing as parsing_test
-#import test.modules.config as config_test
-#import test.modules.scraper as scraper_test
-#import test.modules.database as database_test
-#import test.modules.processing as processing_test
+import test.modules.schema as schema_test
+import test.modules.processing as processing_test
 
 
 def suite():
-    """
-    Create the testsuite.
+    """Create the testsuite.
 
     Returns:
         unittest.TestSuite: runnable testsuite containing all unit tests
+
     """
     test_suite = unittest.TestSuite()
     test_suite.addTest(unittest.makeSuite(parsing_test.TestClass))
-    #test_suite.addTest(unittest.makeSuite(config_test.TestClass))
-    #test_suite.addTest(unittest.makeSuite(scraper_test.TestClass))
-    #test_suite.addTest(unittest.makeSuite(database_test.TestClass))
-    #test_suite.addTest(unittest.makeSuite(processing_test.TestClass))
+    test_suite.addTest(unittest.makeSuite(processing_test.TestClass))
+    test_suite.addTest(unittest.makeSuite(schema_test.TestClass))
     return test_suite
 
 
 def run():
-    """
-    Run the testsuite.
-    """
+    """Run the testsuite."""
     all_suite = suite()
     runner = unittest.TextTestRunner(verbosity=3)
     result = runner.run(all_suite)
