84643e98129b8201abe610316f18cebe4c13f586
Benjamin Fischer <benjamin.f.fischer@fau.de>
2019-12-09 10:22:39 +0100

    Moved source code into src folder.

--- src/mobile/meinbundestag-mobile/src/App.js
+++ src/mobile/meinbundestag-mobile/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { registerRootComponent } from 'expo';
 import { Provider } from 'react-redux';
 
 import storage from './storage/Store';
 import AppNavigator from './navigation/AppNavigator';
 
 
-export default function App() {
+function App() {
   storage.appStore.subscribe(() => console.log(storage.appStore.getState()));
   storage.setProfile('Hans');
   storage.setProfile({ meta: 'meta', speeches: [1, 2, 3] });
@@ -15,3 +16,6 @@ export default function App() {
     </Provider>
   );
 }
+
+
+export default registerRootComponent(App);
