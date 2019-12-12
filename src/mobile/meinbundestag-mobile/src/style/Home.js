e44adc2863c007c73cf8197f42b92db19a0d0f3f
Benjamin Fischer <benjamin.f.fischer@fau.de>
2019-12-12 18:54:27 +0100

    Basic layout of home screen.

---
+++ src/mobile/meinbundestag-mobile/src/style/Home.js
@@ -0,0 +1,62 @@
+import { StyleSheet } from 'react-native';
+import { colorMain } from './Colors';
+
+const home = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+
+  inputView: {
+    flex: 3,
+    alignItems: 'stretch',
+    paddingTop: '30%',
+  },
+
+  input: {
+    borderBottomColor: colorMain,
+    borderBottomWidthWidth: 3,
+    borderRadius: 5,
+    fontSize: 20,
+    alignSelf: 'center',
+    width: '80%',
+  },
+
+  inputContainer: {
+    borderWidth: 0,
+  },
+
+  inputSuggestionText: {
+    textAlign: 'center',
+    margin: 5,
+    fontSize: 14,
+  },
+
+  buttonView: {
+    flex: 1,
+    alignItems: 'center',
+  },
+
+  button: {
+    alignSelf: 'center',
+    fontSize: 30,
+    backgroundColor: colorMain,
+    borderRadius: 30,
+    borderWidth: 2,
+    borderColor: colorMain,
+    padding: 10,
+    margin: 10,
+    width: '33%',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontSize: 24,
+    padding: 10,
+  }
+
+});
+
+export default home;
