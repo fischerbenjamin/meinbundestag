bc012f7a50e7e5b6efd6286cda3212de4186ec56
Benjamin Fischer <benjamin.f.fischer@fau.de>
2019-12-15 18:59:19 +0100

    Updated implementation and display of personal information.

---
+++ src/mobile/meinbundestag-mobile/src/components/SidejobEntry.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+import { entry } from '../style/Profile';
+
+
+export default class SidejobEntry extends React.PureComponent {
+  render() {
+    const {
+      organization, dateStart, dateEnd, job,
+    } = this.props;
+    const timeDate = (() => {
+      if ((dateStart === undefined) || (dateEnd === undefined)) {
+        return undefined;
+      }
+      return `${dateStart} bis ${dateEnd}`;
+    })();
+    return (
+      <View>
+        <View style={entry.container}>
+          <View style={entry.descriptionView}>
+            <Text style={entry.descriptionText}>
+              Organisation
+            </Text>
+          </View>
+          <View style={entry.valueView}>
+            <Text style={entry.valueText}>
+              {organization}
+            </Text>
+          </View>
+        </View>
+        {
+          timeDate !== undefined && (
+            <View style={entry.container}>
+              <View style={entry.descriptionView}>
+                <Text style={entry.descriptionText}>
+                  Zeitraum
+                </Text>
+              </View>
+              <View style={entry.valueView}>
+                <Text style={entry.valueText}>
+                  {timeDate}
+                </Text>
+              </View>
+            </View>
+          )
+        }
+        <View style={entry.container}>
+          <View style={entry.descriptionView}>
+            <Text style={entry.descriptionText}>
+              Amt
+            </Text>
+          </View>
+          <View style={entry.valueView}>
+            <Text style={entry.valueText}>
+              {job}
+            </Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
--- src/mobile/meinbundestag-mobile/src/screens/PersonalScreen.js
+++ src/mobile/meinbundestag-mobile/src/screens/PersonalScreen.js
@@ -2,18 +2,120 @@ import React from 'react';
 import {
   View,
   Text,
+  TouchableOpacity,
+  ScrollView,
+  Clipboard,
 } from 'react-native';
 
-import styles from '../style/Views';
 import { NavIconPersonal } from '../style/Icons';
 import storage from '../storage/Store';
 import BaseScreen from './BaseScreen';
+import PersonalCollapsable from '../components/PersonalCollapsable';
+import {
+  renderSpeech, renderQuestion, renderSidejob, renderVote,
+} from '../components/PersonalEntries';
+import { colorMain } from '../style/Colors';
 
+
+const SIDEJOBS = 'sidejobs';
+const SPEECHES = 'speeches';
+const QUESTIONS = 'questions';
+const VOTES = 'votes';
 
 export default class PersonalScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showContent: false,
+    };
+  }
+
   static navigationOptions = {
     tabBarIcon: NavIconPersonal,
   };
+
+  showOverview() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          alignItems: 'stretch',
+          justifyContent: 'space-around',
+        }}
+      >
+        {this.renderOverviewItem(SIDEJOBS, 'Nebentätigkeiten')}
+        {this.renderOverviewItem(SPEECHES, 'Reden')}
+        {this.renderOverviewItem(QUESTIONS, 'Fragen')}
+        {this.renderOverviewItem(VOTES, 'Abstimmungen')}
+      </View>
+    );
+  }
+
+  renderOverviewItem(content, text) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: colorMain,
+          borderRadius: 30,
+          margin: 20,
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => this.setState({
+            showContent: content,
+          })}
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Text
+            style={{
+              fontWeight: 900,
+              fontSize: 22,
+              color: '#fff',
+            }}
+          >
+            {text}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  renderContent(data, renderListItem, onPressItem) {
+    return (
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+      >
+        <TouchableOpacity
+          onPress={() => this.setState({ showContent: false })}
+        >
+          <View
+            style={{
+              alignSelf: 'center',
+              alignItems: 'center',
+              padding: 10,
+              backgroundColor: colorMain,
+              borderRadius: 20,
+              margin: 10,
+              width: '50%',
+            }}
+          >
+            <Text>Zurück zur Übersicht</Text>
+          </View>
+        </TouchableOpacity>
+        <PersonalCollapsable
+          data={data}
+          renderListItem={renderListItem}
+          onPressItem={onPressItem}
+        />
+      </ScrollView>
+    );
+  }
 
   render() {
     const { profile } = storage.getProfile();
@@ -22,12 +124,37 @@ export default class PersonalScreen extends React.Component {
         'Bitte wählen Sie zuerst ein Profil aus.',
       );
     }
-    return (
-      <View style={styles.container.basic.centerAll}>
-        <Text>
-          Profil wurde ausgewählt.
-        </Text>
-      </View>
-    );
+    const {
+      sidejobs, votes, questions, speeches,
+    } = profile;
+    const { showContent } = this.state;
+    const { navigate } = this.props.navigation;
+    switch (showContent) {
+      case SIDEJOBS:
+        return this.renderContent(
+          sidejobs, renderSidejob,
+          ((item) => Clipboard.setString(item.organization)),
+        );
+      case SPEECHES:
+        return this.renderContent(
+          speeches, renderSpeech,
+          ((item) => {
+            storage.setSpeech(item);
+            navigate('speech');
+          }),
+        );
+      case VOTES:
+        return this.renderContent(
+          votes, renderVote,
+          (item) => Clipboard.setString(item.url),
+        );
+      case QUESTIONS:
+        return this.renderContent(
+          questions, renderQuestion,
+          ((item) => Clipboard.setString(item.url)),
+        );
+      default:
+        return this.showOverview();
+    }
   }
 }
