c1e9a13127ee2b22e9b2e2b8f32167d4dbc4204a
Benjamin Fischer <benjamin.f.fischer@fau.de>
2020-01-05 20:42:31 +0100

    Documentation

--- src/mobile/meinbundestag-mobile/src/navigation/AppNavigator.js
+++ src/mobile/meinbundestag-mobile/src/navigation/AppNavigator.js
@@ -1,7 +1,17 @@
+/**
+ * @file Navigation object for the whole application
+ * @author Benjamin Fischer
+ *
+ * Wrapper for the navigation container. The BottomTabNavigator could be
+ * replaced by another type of navigator.
+ */
+
 import { createAppContainer } from 'react-navigation';
+
 import BottomTabNavigator from './BottomTabNavigator';
 
 
+// Create the app navigator based on the bottomtabnavigator
 const AppNavigator = createAppContainer(BottomTabNavigator);
 
 
--- src/mobile/meinbundestag-mobile/src/navigation/BottomTabNavigator.js
+++ src/mobile/meinbundestag-mobile/src/navigation/BottomTabNavigator.js
@@ -1,3 +1,11 @@
+/**
+ * @file Implements a BottomTabNavigator for the applicaton
+ * @author Benjamin Fischer
+ *
+ * Connects the four screens to the corresponding data in the redux store so
+ * that each screen automatically re-renders when the data changes.
+ */
+
 import { connect } from 'react-redux';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
@@ -9,23 +17,28 @@ import PersonalScreen from '../screens/Personal/PersonalScreen';
 import { colorMain } from '../style/Colors';
 
 
+// HomeScreen
 const HomeScreenConnected = connect((state) => (
   { profile: state.profile }
 ))(HomeScreen);
 
+// ProfileScreen
 const ProfileScreenConnected = connect((state) => (
   { profile: state.profile }
 ))(ProfileScreen);
 
+// PersonalScreen
 const PersonalScreenConnected = connect((state) => ({
   profile: state.profile,
   personalContent: state.personalContent,
 }))(PersonalScreen);
 
+// SpeechScreen
 const SpeechScreenConnected = connect((state) => (
   { profile: state.speech }
 ))(SpeechScreen);
 
+// Create the BottomTabNavigator
 const BottomTabNavigator = createBottomTabNavigator(
   {
     home: HomeScreenConnected,
